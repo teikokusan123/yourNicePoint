fc1f53b95c8b97092bbe2a198b07b8a5aa8e6ced
teikokusan123 <125336251+teikokusan123@users.noreply.github.com>
2023-07-04 10:45:46 +0900

    ver.1.1.3
    藤井が含まれていると特定の文が出るようにしました

--- assessment.js
+++ assessment.js
@@ -78,9 +78,9 @@ function assessment(userName)
   const index = sumOfCharCode % answers.length;
   let result = answers[index];
   result = result.replaceAll('###userName###', userName);
-  if(userName === "藤井")
+  if(userName.includes("藤井"))
   {
-    result = "藤井のいいところはありません。藤井にはいいところなど存在しないのです";
+    result = userName + "のいいところはありません。" + userName + "にはいいところなど存在しないのです";
   }
   return result;
 }
